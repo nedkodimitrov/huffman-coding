be20211e6b537e892cc945a37f5e121d681d6179
Nedko Dimitrov <122704901+nedkodimitrov@users.noreply.github.com>
2023-11-25 17:43:42 +0200

    Implement a function to read a character from a file.

--- decode.c
+++ decode.c
@@ -1,7 +1,12 @@
 #include "common.h"
 
+
+// Read a char bit by bit using readBitFromFile(). Returns EOF if unsucessful.
+int readCharFromFile(FILE *fp_in_file, char *byte);
+
 // Reads a byte from a file and returns a bit of the byte on every call. Returns EOF if unsucessful.
 int readBitFromFile(FILE *fp_in_file, char *bit);
+
 
 int main()
 {
@@ -17,12 +22,47 @@ int main()
     fread(&tree_size_read, sizeof(tree_size_read), 1, fp_in_file);
     printf("tree size: %d\n", tree_size_read);
 
+    char bit;
     char byte;
-    while (readBitFromFile(fp_in_file, &byte) != EOF)
-    {
-        printf("%c", byte + '0');
-    }
+
+    // 1t
+    readBitFromFile(fp_in_file, &bit);
+    printf("%c ", bit + '0');
+    readCharFromFile(fp_in_file, &byte);
+    printf("%c ", byte);
+
+    // 1a
+    readBitFromFile(fp_in_file, &bit);
+    printf("%c ", bit + '0');
+    readCharFromFile(fp_in_file, &byte);
+    printf("%c ", byte);
+
+    // 1r
+    readBitFromFile(fp_in_file, &bit);
+    printf("%c ", bit + '0');
+    readCharFromFile(fp_in_file, &byte);
+    printf("%c ", byte);
+
+    //root = ReconstructHuffmanTree(fp_in_file);
     
+    return 0;
+}
+
+
+// Read a char bit by bit using readBitFromFile(). Returns EOF if unsucessful.
+int readCharFromFile(FILE *fp_in_file, char *byte)
+{
+    char bit;
+    // Need to read it bit by bit so that it doesn't get read from the file before some other bits that have not filled a byte yet.
+    for (int j = CHAR_BIT - 1; j >= 0; j--)
+    {
+        if (readBitFromFile(fp_in_file, &bit) == EOF)
+        {
+            return EOF;
+        }
+
+        *byte = ((*byte << 1) | bit);
+    }
     return 0;
 }
 
